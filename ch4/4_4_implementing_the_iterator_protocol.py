5523707fc720494f1830a56845edd3774744f3f9
Julio Briones <Julio_Briones@epam.com>
2022-04-28 07:50:47 -0500

    Worked up to 4.5.

--- ch4/4_4_implementing_the_iterator_protocol.py
+++ ch4/4_4_implementing_the_iterator_protocol.py
@@ -33,6 +33,62 @@ class Node:
             yield from c.depth_first()
 
 
+class Node2:
+    def __init__(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return "Node2({!r})".format(self._value)
+
+    def add_child(self, other_node):
+        self._children.append(other_node)
+
+    def __iter__(self):
+        return iter(self._children)
+
+    def depth_first(self):
+        return DepthFirstIterator(self)
+
+
+class DepthFirstIterator(object):
+    """Depth-first traversal"""
+
+    def __init__(self, start_node):
+        self._node = start_node
+        self._children_iter = None
+        self._child_iter = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # Return myself if just started, create an iterator for children
+        if self._children_iter is None:
+            self._children_iter = iter(self._node)
+            return self._node
+
+        # If processing a child, return its next item
+        elif self._child_iter:
+            try:
+                next_child = next(self._child_iter)
+                return next_child
+            except StopIteration:
+                self._child_iter = None
+                return next(self)
+
+        # Advance to the next child and start its iteration
+        else:
+            self._child_iter = next(self._children_iter).depth_first()
+
+
+"""
+The DepthFirstIterator class works in the same way as the generator version, but it’s
+a mess because the iterator has to maintain a lot of complex state about where it is in
+the iteration process. Frankly, nobody likes to write mind-bending code like that. Define
+your iterator as a generator and be done with it.
+"""
+
 if __name__ == "__main__":
     root = Node(0)
     child1 = Node(1)
---
+++ ch4/4_5_iterating_in_reverse.py
@@ -0,0 +1,9 @@
+"""
+4.5. Iterating in Reverse
+
+Problem
+    You want to iterate in reverse over a sequence.
+
+Solution
+    Use the built-in reversed() function.
+"""
